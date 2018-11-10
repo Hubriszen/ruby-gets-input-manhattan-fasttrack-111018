459333ce53f43155c3991a35479852a1cd5d9dfd
M H <ptcb795@gmail.com>
2018-11-10 19:55:02 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,6 @@
-def greeting
+def greeting{name}
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "Please enter your name so thatwe can greet you more personaly:"
+  name=gets.strip
+  greeting(name)
 end
